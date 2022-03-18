bea742cea7b0639e4daaef5f7859b0d1efbeb3f9
Duncan MacGregor <duncan.macgregor@oracle.com>
2022-03-18 16:53:42 +0000

    Add tests for condition variables with fiber scheduling.

--- test/mri/excludes/TestFiberMutex.rb
+++ test/mri/excludes/TestFiberMutex.rb
@@ -1,4 +1,3 @@
 exclude :test_mutex_deadlock, "Fiber.set_scheduler"
-exclude :test_condition_variable, "Fiber.set_scheduler"
 exclude :test_queue, "Fiber.set_scheduler"
 exclude :test_queue_pop_waits, "hangs"
