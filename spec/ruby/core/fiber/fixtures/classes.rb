bed1496c74594fb7d3dfd66ef1e02edfb2b3e849
Duncan MacGregor <duncan.macgregor@oracle.com>
2022-03-18 13:27:47 +0000

    Tests for basic fiber scheduler functionality.

--- spec/ruby/core/fiber/fixtures/classes.rb
+++ spec/ruby/core/fiber/fixtures/classes.rb
@@ -1,4 +1,36 @@
+require 'fiber'
+require 'socket'
+
+begin
+  require 'io/nonblock'
+rescue LoadError
+  # Ignore.
+end
+
 module FiberSpecs
+
+  class EmptyScheduler
+
+    def io_wait(io, events, duration)
+      Fiber.yield
+      return true
+    end
+
+    def block(blocker, timeout = nil)
+      Fiber.yield
+    end
+
+    def unblock(blocker, fiber)
+    end
+
+    def fiber(&block)
+      fiber = Fiber.new(blocking: false, &block)
+
+      fiber.resume
+
+      return fiber
+    end
+  end
 
   class NewFiberToRaise
     def self.raise(*args)
--- test/mri/excludes/TestFiberSleep.rb
+++
@@ -1,3 +0,0 @@
-exclude :test_sleep, "needs investigation"
-exclude :test_sleep_returns_seconds_slept, "needs investigation"
-exclude :test_broken_sleep, "needs investigation"
