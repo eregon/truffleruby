ddb59d8d27cc570a1f8958ae6f20562716a79bac
Duncan MacGregor <duncan.macgregor@oracle.com>
2022-03-21 10:52:34 +0000

    Add specs for queue and sized queue with fiber scheduling.

---
+++ spec/ruby/core/fiber/queue_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../../spec_helper'
+require_relative "fixtures/classes"
+
+ruby_version_is "3.0" do
+  describe "A non blocking fiber with a scheduler" do
+    it "calls `block` on the scheduler when attempting to pop from an empty queue" do
+      queue = Queue.new
+      popped_value = nil
+      scheduler = FiberSpecs::BlockUnblockScheduler.new
+      Thread.new do
+        Fiber.set_scheduler scheduler
+
+        Fiber.schedule do
+          popped_value = queue.pop
+        end
+        queue.push :foo
+      end.join
+
+      popped_value.should == :foo
+      scheduler.block_calls.should == 1
+      scheduler.unblock_calls.should == 1
+    end
+  end
+end
--- test/mri/excludes/TestFiberMutex.rb
+++ test/mri/excludes/TestFiberMutex.rb
@@ -1,3 +1,1 @@
 exclude :test_mutex_deadlock, "Fiber.set_scheduler"
-exclude :test_queue, "Fiber.set_scheduler"
-exclude :test_queue_pop_waits, "hangs"
